2dcf4a21f94992953f2a66ae3a024f53d372ae01
FSFbot <felipe.sfreitas14@gmail.com>
2024-09-07 18:31:39 -0300

    v1 da criação de usuario login

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './stylesComponents/LoginModal.css';
 
+import { useNavigate } from 'react-router-dom';
+import { login } from '../Service/Service';
+import useLocalStorage from "../hooks/useLocalStorage";
+
 const LoginModal = ({ closeLogin, showSignup }) => {
+    const navigate = useNavigate();
+    const [token, setToken] = useLocalStorage('token', ''); // Use um valor padrão vazio para o token
+    const [userLogin, setUserLogin] = useState({
+        id: 0,
+        usuario: '',
+        senha: '',
+        token: ''
+    });
+
+    function updatedModel(e) {
+        setUserLogin({
+            ...userLogin,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    useEffect(() => {
+        if (token !== '') {
+            navigate('/home');
+        }
+    }, [token, navigate]);
+
+    async function onSubmit(e) {
+        e.preventDefault();
+        try {
+            await login('/auth', userLogin, setToken);
+            alert('Usuário logado com sucesso!');
+        } catch (error) {
+            alert('Dados do usuário inconsistentes. Erro ao logar!');
+        }
+    }
+
+
     return (
         <div className="modal-overlay" onClick={(e) => e.target.className === 'modal-overlay' && closeLogin()}>
             <div className="modal-content">
