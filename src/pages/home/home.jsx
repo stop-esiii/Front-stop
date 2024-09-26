15d79725f3e2c52c1f222c6ac9d5f0509dc9734a
Felipe Sousa <felipe.fps09@hotmail.com>
2024-09-26 11:53:22 -0300

    Fix: Ajuste de nome de pasta para padrão

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -7,7 +7,7 @@ import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
-import './home.css'; 
+import './Home.css'; 
 
 function Home() {
   const navigate = useNavigate();
