9716fa10f6fe94194f2313cd162af331a5e58022
Felipe Sousa <felipe.fps09@hotmail.com>
2024-09-26 21:18:38 -0300

    Fix: Renomeando serviços para que seja somente request e outras utilidades.

---
+++ src/services/Requests.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const API_URL = 'https://stop-backend.up.railway.app/api';
+
+export const loginRequest = async (credentials) => {
+  return axios.post(`${API_URL}/auth`, credentials)
+    .then(response => response.data)
+    .catch(error => {
+      console.error('Erro ao fazer login:', error);
+      throw error;
+    });
+};
+
+export const registerRequest = async (requestBody) => {
+  return axios.post(`${API_URL}/users`, requestBody)
+    .then(response => response.data)
+    .catch(error => {
+      console.error('Erro ao fazer login:', error);
+      throw error;
+    });
+}
+
+// Outras funções de serviço podem ser adicionadas aqui
