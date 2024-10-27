e7ad80bad234dde6afcf0c21e71de442f74797a0
Felipe Sousa <felipe.fps09@hotmail.com>
2024-10-27 12:23:50 -0300

    Fix: restrições de rotas;

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
@@ -11,6 +11,7 @@ import DrawLetter from './pages/DrawLetter/DrawLetter';
 import GameScreen from './pages/GameScreen/GameScreen';
 import StopScreen from './pages/StopScreen/StopScreen';
 import ValidationScreen from './pages/ValidationScreen/ValidationScreen'; // Importar a nova página
+import { getItem } from './services/StorageService';
 
 const theme = createTheme({
   palette: {
@@ -24,24 +25,76 @@ const theme = createTheme({
 });
 
 function App() {
+  function PrivateRoute({ children }) {
+    const token = getItem('userInfo');
+    return token ? children : <Navigate to="/" />;
+  }
+
   return (
-      <ThemeProvider theme={theme}>
-        <Router>
-          <div className="App">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/game-options" element={<GameOptions />} />
-              <Route path="/draw-letter" element={<DrawLetter />} />
-              <Route path="/game-screen" element={<GameScreen />} />
-              <Route path="/stop" element={<StopScreen />} />
-              <Route path="/validation" element={<ValidationScreen />} /> {/* Nova rota */}
-            </Routes>
-          </div>
-        </Router>
-      </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+
+            {/* Rotas protegidas */}
+            <Route
+              path="/profile"
+              element={
+                <PrivateRoute>
+                  <Profile />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/game-options"
+              element={
+                <PrivateRoute>
+                  <GameOptions />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/draw-letter"
+              element={
+                <PrivateRoute>
+                  <DrawLetter />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/game-screen"
+              element={
+                <PrivateRoute>
+                  <GameScreen />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/stop"
+              element={
+                <PrivateRoute>
+                  <StopScreen />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/validation"
+              element={
+                <PrivateRoute>
+                  <ValidationScreen />
+                </PrivateRoute>
+              }
+            />
+
+            {/* Redirecionar rotas inválidas */}
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
