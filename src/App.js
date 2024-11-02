af6ba34fd20158c97a65e4608807401f96e016fa
FSFbot <felipe.sfreitas14@gmail.com>
2024-11-02 18:35:36 -0300

    versão do dia 02/11

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Home from './pages/Home/Home';
+import Home from './pages/home/Home';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import Profile from './pages/Profile/Profile';
@@ -10,7 +11,7 @@ import DrawLetter from './pages/DrawLetter/DrawLetter';
 import GameScreen from './pages/GameScreen/GameScreen';
 import StopScreen from './pages/StopScreen/StopScreen';
 import ValidationScreen from './pages/ValidationScreen/ValidationScreen';
-import WaitingRoom from './pages/WaitingRoom/WaitingRoom'; // Nova importação
+import WaitingRoom from './pages/WaitingRoom/WaitingRoom'; // Importar a nova página
 
 const theme = createTheme({
   palette: {
@@ -25,24 +26,24 @@ const theme = createTheme({
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/game-options" element={<GameOptions />} />
-            <Route path="/waiting-room" element={<WaitingRoom />} /> {/* Nova rota */}
-            <Route path="/draw-letter" element={<DrawLetter />} />
-            <Route path="/game-screen" element={<GameScreen />} />
-            <Route path="/stop" element={<StopScreen />} />
-            <Route path="/validation" element={<ValidationScreen />} />
-          </Routes>
-        </div>
-      </Router>
-    </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/game-options" element={<GameOptions />} />
+              <Route path="/draw-letter" element={<DrawLetter />} />
+              <Route path="/game-screen" element={<GameScreen />} />
+              <Route path="/stop" element={<StopScreen />} />
+              <Route path="/validation" element={<ValidationScreen />} />
+              <Route path="/waiting-room" element={<WaitingRoom />} /> {/* Nova rota */}
+            </Routes>
+          </div>
+        </Router>
+      </ThemeProvider>
   );
 }
 
